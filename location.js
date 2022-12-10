b1d2d904d2c6382f6566d132dc8b62f6b8edd198
GarayevF <112488747+GarayevF@users.noreply.github.com>
2022-12-10 07:01:12 +0400

    Add files via upload

--- location.js
+++ location.js
@@ -2,14 +2,22 @@ let bigmap = document.querySelector('#bigmap')
 let map = document.querySelector('#map')
 let boyut = document.querySelector('#boyut')
 let bagla = document.querySelector('#bagla')
+let goback = document.querySelector('#goback');
+let title = document.querySelector('#title');
 
 boyut.addEventListener('click', function() {
-    bigmap.classList.remove('d-none');
+    // bigmap.classList.remove('d-none');
+    bigmap.style.height = '700px'
     map.classList.add('d-none');
+    goback.classList.add('d-none')
+    title.classList.add('d-none')
 })
 
 
 bagla.addEventListener('click', function() {
-    bigmap.classList.add('d-none');
+    // bigmap.classList.add('d-none');
+    bigmap.style.height = '0'
     map.classList.remove('d-none');
+    goback.classList.remove('d-none')
+    title.classList.remove('d-none')
 })
